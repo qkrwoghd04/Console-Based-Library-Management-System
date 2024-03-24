368054b61f93ddc88f521c8f44feb0573edb53c9
JDEV <qkrwoghd04@naver.com>
2024-03-24 16:25:14 +0800

    Update DatabaseConnection class

--- code/library/DatabaseConnection.java
+++ code/library/DatabaseConnection.java
@@ -3,6 +3,8 @@ package library;
 import java.util.HashMap;
 import java.util.Map;
 
+// Using singleton pattern 
+// The constructor of the class is declared as private to prevent instantiation of the class from the outside
 public class DatabaseConnection {
   private static DatabaseConnection instance;
   private Map<String, User> users;
